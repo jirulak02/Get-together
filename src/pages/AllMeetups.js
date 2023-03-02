e0c0ddb4827cb5ed0b1c2ec695c01c1635c96dee
Jiří Šimeček <109624095+jirulak02@users.noreply.github.com>
2023-03-02 16:48:06 +0100

    update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -9,7 +9,7 @@ function AllMeetupsPage() {
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      'https://react-course-project2-633a6-default-rtdb.europe-west1.firebasedatabase.app/meetups.json'
+      '<<FIREBASE REFERENCE URL>>/meetups.json'
     ).then(response => {
       return response.json();
     }).then(data => {
